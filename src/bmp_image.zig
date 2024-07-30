a38cf9562fc36f52bbdf2904553b98634f138db6
Timothy Cherney <tjcherney@hotmail.com>
2024-07-29 17:13:53 -0700

    implemented rough bmp handling, currently only works uncompressed 24 bpp os2 header bmps like the ones im outputting

--- src/bmp_image.zig
+++ src/bmp_image.zig
@@ -4,13 +4,18 @@ const utils = @import("utils.zig");
 
 pub const BMPImage_Error = error{
     NOT_LOADED,
+    INVALID_BMP_HEADER,
+    INVALID_DIB_HEADER,
 };
 
 pub const BMPImage = struct {
-    _file_data: utils.ByteStream = undefined,
+    _file_data: utils.BitReader = undefined,
     _allocator: *std.mem.Allocator = undefined,
     _loaded: bool = false,
     data: std.ArrayList(utils.Pixel(u8)) = undefined,
+    _file_size: u32 = undefined,
+    _bpp: u32 = undefined,
+    _offset: u32 = undefined,
     width: u32 = undefined,
     height: u32 = undefined,
 
@@ -28,9 +33,14 @@ pub const BMPImage = struct {
     }
     pub fn load_PNG(self: *BMPImage, file_name: []const u8, allocator: *std.mem.Allocator) !void {
         self._allocator = allocator;
-        self._file_data = utils.ByteStream{};
-        try self._file_data.init_file(file_name, self._allocator);
+        self._file_data = utils.BitReader{};
+        try self._file_data.init_file(file_name, self._allocator, .{ .little_endian = true });
         std.debug.print("reading bmp\n", .{});
+        try self.read_BMP_header();
+        try self.read_DIB_header();
+        self.data = try std.ArrayList(utils.Pixel(u8)).initCapacity(self._allocator.*, self.height * self.width);
+        self.data.expandToCapacity();
+        try self.read_color_data();
         self._loaded = true;
     }
 
@@ -38,9 +48,74 @@ pub const BMPImage = struct {
         return &self.data.items[y * self.width + x];
     }
 
+    fn read_color_data(self: *BMPImage) (utils.ByteStream_Error || utils.BitReader_Error || BMPImage_Error)!void {
+        const padding_size = self.width % 4;
+        var i: usize = self.height - 1;
+        var j: usize = 0;
+        while (i >= 0) {
+            while (j < self.width) {
+                if (self._bpp == 24) {
+                    self.data.items[i * self.width + j] = utils.Pixel(u8){
+                        .b = try self._file_data.read_byte(),
+                        .g = try self._file_data.read_byte(),
+                        .r = try self._file_data.read_byte(),
+                    };
+                    j += 1;
+                }
+            }
+            for (0..padding_size) |_| {
+                _ = try self._file_data.read_byte();
+            }
+            j = 0;
+            if (i == 0) break;
+            i -= 1;
+        }
+    }
+
+    fn read_DIB_header(self: *BMPImage) (utils.ByteStream_Error || utils.BitReader_Error || BMPImage_Error)!void {
+        const header_size = try self._file_data.read_int();
+        std.debug.print("header_size {d}\n", .{header_size});
+        // OS/2
+        if (header_size == 12) {
+            self.width = try self._file_data.read_word();
+            self.height = try self._file_data.read_word();
+            const color_planes = try self._file_data.read_word();
+            if (color_planes != 1) {
+                return BMPImage_Error.INVALID_DIB_HEADER;
+            }
+            self._bpp = try self._file_data.read_word();
+            std.debug.print("width {d}, height {d}, color_planes {d}, bpp {d}\n", .{ self.width, self.height, color_planes, self._bpp });
+        }
+        //TODO handle other headers
+        else {
+            return BMPImage_Error.INVALID_DIB_HEADER;
+        }
+    }
+
+    fn read_BMP_header(self: *BMPImage) (utils.ByteStream_Error || utils.BitReader_Error || BMPImage_Error)!void {
+        var bmp_type: [2]u8 = [_]u8{0} ** 2;
+        // type
+        bmp_type[0] = try self._file_data.read_byte();
+        bmp_type[1] = try self._file_data.read_byte();
+        //TODO handle other headers
+        if (!std.mem.eql(u8, &bmp_type, "BM")) {
+            return BMPImage_Error.INVALID_BMP_HEADER;
+        }
+        std.debug.print("file type {s}\n", .{bmp_type});
+        // size
+        self._file_size = try self._file_data.read_int();
+        std.debug.print("file size {d}\n", .{self._file_size});
+        // reserved
+        _ = try self._file_data.read_word();
+        _ = try self._file_data.read_word();
+        // offset
+        self._offset = try self._file_data.read_int();
+        std.debug.print("offset {d}\n", .{self._offset});
+    }
+
     pub fn write_BMP(self: *BMPImage, file_name: []const u8) !void {
         if (!self._loaded) {
-            return BMPImage.NOT_LOADED;
+            return BMPImage_Error.NOT_LOADED;
         }
         const image_file = try std.fs.cwd().createFile(file_name, .{});
         defer image_file.close();
@@ -88,3 +163,15 @@ pub const BMPImage = struct {
         std.ArrayList(utils.Pixel(u8)).deinit(self.data);
     }
 };
+
+test "BASIC 16" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var allocator = gpa.allocator();
+    var image = BMPImage{};
+    try image.load_PNG("tests/bmp/parrot.bmp", &allocator);
+    try image.write_BMP("parrot2.bmp");
+    image.deinit();
+    if (gpa.deinit() == .leak) {
+        std.debug.print("Leaked!\n", .{});
+    }
+}
